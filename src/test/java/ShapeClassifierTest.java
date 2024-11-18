f991c770fe26b6100b13ff8bb7bade6b8dcc997b
Pranav Rihal <pranavr753@gmail.com>
2024-11-18 12:00:29 -0500

    adding blackbox tests purely via gpt

--- src/test/java/ShapeClassifierTest.java
+++ src/test/java/ShapeClassifierTest.java
@@ -1,6 +1,9 @@
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.DisplayName;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertSame;
 
 public class ShapeClassifierTest {
 
@@ -13,5 +16,21 @@ public class ShapeClassifierTest {
         ShapeClassifier s = new ShapeClassifier();
         String answer = s.evaluateGuess("Equilateral,Large,Yes,100,100,100");
         assertNotEquals("Yes", answer);
+    }
+
+    @DisplayName("Example Guess Test")
+    @Test
+    public void incorrectShapeGuess() {
+        ShapeClassifier s = new ShapeClassifier();
+        String answer = s.evaluateGuess("Rectangle,Large,Yes,100,100,100");
+        assertEquals("Yes", answer);
+    }
+
+    @DisplayName("Example Guess Test")
+    @Test
+    public void incorrectSizeGuess() {
+        ShapeClassifier s = new ShapeClassifier();
+        String answer = s.evaluateGuess("Equilateral,Small,Yes,100,100,100");
+        assertEquals("Yes", answer);
     }
 }
